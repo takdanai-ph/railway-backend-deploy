3fcea17a6a706298d340eb37487f0a06f2aee7c7
takdanai_ph <bestpimharn@gmail.com>
2025-03-17 17:15:52 +0700

    fixing data

--- models/User.js
+++ models/User.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
     password_hash: { type: String, required: true },
     role: { type: String, enum: ['Admin', 'Manager', 'User'], default: 'User' },
     profile_picture: { type: String, default: '' },
-    team_id: { type: mongoose.Schema.Types.UUID, ref: 'Team', default: null },
+    team_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', default: null },
     assigned_tasks: [{ type: mongoose.Schema.Types.UUID, ref: 'Task' }],
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
