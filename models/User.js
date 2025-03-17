a3fe19e462ae85ac2a3fa2b88818c0c6a247f155
takdanai_ph <bestpimharn@gmail.com>
2025-03-17 16:50:55 +0700

    Add users

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.UUID, default: () => new mongoose.Types.UUID(), unique: true },
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password_hash: { type: String, required: true },
+    role: { type: String, enum: ['Admin', 'Manager', 'User'], default: 'User' },
+    profile_picture: { type: String, default: '' },
+    team_id: { type: mongoose.Schema.Types.UUID, ref: 'Team', default: null },
+    assigned_tasks: [{ type: mongoose.Schema.Types.UUID, ref: 'Task' }],
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('User', UserSchema)
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/User.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (req, res, next) => {
+    User.find((err, users) => {
+        if (err) return next(err);
+        res.json(users);
+    })
+})
+
+router.get('/:id', (req, res, next) => {
+    User.findById(req.params.id, (err, post) => {
+        if (err) return next(err);
+        res.json(post);
+    })
+})
+
+router.post('/', (req, res, next) => {
+    User.create(req.body, (err, post) => {
+        if (err) return next(err);
+        res.json(post);
+    })
+})
+
+router.put('/:id', (req, res, next) => {
+    User.findByIdAndUpdate(req.params.id, req.body, (err, post) => {
+        if (err) return next(err);
+        res.json(post);
+    })
+})
+
+router.delete('/:id', (req, res, next) => {
+    User.findByIdAndDelete(req.params.id, (err, post) => {
+        if (err) return next(err);
+        res.json(post);
+    })
+})
 
 module.exports = router;
