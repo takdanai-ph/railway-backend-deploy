6509c3da57c90237a9fd689a5100023e3cd94300
takdanai_ph <bestpimharn@gmail.com>
2025-03-17 17:31:31 +0700

    fixing data

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
 const UserSchema = new mongoose.Schema({
-    user_id: { type: String, default: uuidv4, unique: true },
+    user_id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     fname: { type: String, required: true },
     lname: { type: String, required: true },
     username: { type: String, required: true, unique: true },
